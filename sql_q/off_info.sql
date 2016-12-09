018d0e1f160e4b6de6b1da04b24a211198e53301
shreyas10p <shreyas10p@gmail.com>
2016-12-09 17:47:16 +0530

    add file

---
+++ sql_q/off_info.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 09, 2016 at 12:21 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `offices`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `off_info`
+--
+
+CREATE TABLE IF NOT EXISTS `off_info` (
+  `emp_id` int(11) NOT NULL AUTO_INCREMENT,
+  `emp_data` varchar(255) NOT NULL,
+  PRIMARY KEY (`emp_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=40 ;
+
+--
+-- Dumping data for table `off_info`
+--
+
+INSERT INTO `off_info` (`emp_id`, `emp_data`) VALUES
+(1, '{"Code":"","City":"","Address1":"","Address2":"","dropdown":"Maharashtra","Territory":""}'),
+(2, '{"Code":"","City":"","Address1":"","Address2":"","dropdown":"Maharashtra","Territory":""}'),
+(3, '{"Code":"","City":"","Address1":"","Address2":"","dropdown":"Maharashtra","Territory":""}'),
+(4, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(5, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(6, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(7, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(8, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(9, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(10, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(11, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(12, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(13, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(14, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(15, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(16, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(17, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(18, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(19, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(20, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(21, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(22, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(23, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(24, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(25, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(26, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(27, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(28, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(29, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(30, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(31, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(32, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(33, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(34, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(35, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(36, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(37, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(38, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}'),
+(39, '{"Code":"1212","City":"sda","Address1":"sdas","Address2":"sda","dropdown":"Maharashtra","Territory":"sdfad"}');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
